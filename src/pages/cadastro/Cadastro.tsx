50ba8850e0f87bc48a23815ce15d0f02121902a0
Alissa Sousa <alissiasuelen@hotmail.com>
2026-02-13 18:44:10 -0300

    Implementação do AuthContext.tsx

--- src/pages/cadastro/Cadastro.tsx
+++ src/pages/cadastro/Cadastro.tsx
@@ -1,79 +1,81 @@
-import { useEffect, useState, type ChangeEvent, type FormEvent } from "react"
-import { useNavigate } from "react-router-dom"
-import { ClipLoader } from "react-spinners"
-import type Usuario from "../../models/Usuario"
-import { cadastrarUsuario } from "../../services/Service"
+import { useEffect, useState, type ChangeEvent, type FormEvent } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { ClipLoader } from 'react-spinners';
+import type Usuario from '../../models/Usuario';
+import { cadastrarUsuario } from '../../services/Service';
 
 function Cadastro() {
+  const navigate = useNavigate();
 
-const navigate = useNavigate()
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
-const [isLoading, setIsLoading] = useState<boolean>(false)
+  const [confirmarSenha, setConfirmaSenha] = useState<string>('');
 
-const[confirmarSenha, setConfirmaSenha] = useState<string>('')
-
-const [usuario, setUsuario] = useState<Usuario>({
+  const [usuario, setUsuario] = useState<Usuario>({
     id: 0,
     nome: '',
     usuario: '',
     senha: '',
-    foto: ''
-})
+    foto: '',
+  });
 
-useEffect(() => {
-    if (usuario.id !== 0){
-        retornar()
+  function retornar() {
+    navigate('/login');
+  }
+
+  useEffect(() => {
+    if (usuario.id !== 0) {
+      retornar();
     }
-}, [usuario])
+  }, [usuario]);
 
-function retornar(){
-    navigate('/login')
-}
-
-function atualizarEstado(e: ChangeEvent<HTMLInputElement>){
+  function atualizarEstado(e: ChangeEvent<HTMLInputElement>) {
     setUsuario({
-        ...usuario,
-        [e.target.name]: e.target.value
-    })
-}
+      ...usuario,
+      [e.target.name]: e.target.value,
+    });
+  }
 
-function handleConfirmarSenha(e: ChangeEvent<HTMLInputElement>){
-    setConfirmaSenha(e.target.value)
-}
+  function handleConfirmarSenha(e: ChangeEvent<HTMLInputElement>) {
+    setConfirmaSenha(e.target.value);
+  }
 
-async function cadastrarNovoUsuario(e: FormEvent<HTMLFormElement>){
-    e.preventDefault()
+  async function cadastrarNovoUsuario(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
 
-    if(confirmarSenha === usuario.senha && usuario.senha.length >= 8){
+    if (confirmarSenha === usuario.senha && usuario.senha.length >= 8) {
+      setIsLoading(true);
 
-        setIsLoading(true)
-
-        try{
-            await cadastrarUsuario('/usuarios/cadastrar', usuario, setUsuario)
-            alert('Usuário cadastrado com sucesso!')
-        }catch(error){
-            alert('Erro ao cadastrar o usuário!')
-        }
-    }else{
-        alert('Dados do usuário inconsistentes! Verifique as informações do cadastro.')
-        setUsuario({...usuario, senha: ''})
-        setConfirmaSenha('')
+      try {
+        await cadastrarUsuario('/usuarios/cadastrar', usuario, setUsuario);
+        alert('Usuário cadastrado com sucesso!');
+      } catch (error) {
+        alert('Erro ao cadastrar o usuário!');
+      }
+    } else {
+      alert(
+        'Dados do usuário inconsistentes! Verifique as informações do cadastro.'
+      );
+      setUsuario({ ...usuario, senha: '' });
+      setConfirmaSenha('');
     }
 
-    setIsLoading(false)
-}
+    setIsLoading(false);
+  }
 
   return (
     <>
-    <div className="grid grid-cols-1 lg:grid-cols-2 h-screen place-items-center font-bold">
-      <div
-        className= "bg-[url('"
-      
-      ></div>
-      <form className="flex justify-center items-center flex-col w-2/3 gap-3"
-            onSubmit={cadastrarNovoUsuario}>
-        <h2 className="text-slate-900 text-5xl">Cadastrar</h2>
-        <div className="flex flex-col w-full">
+      <div className="grid grid-cols-1 lg:grid-cols-2 h-screen place-items-center font-bold">
+        <div
+          className="bg-[url('https://i.imgur.com/ZZFAmzo.jpg')] lg:block hidden bg-no-repeat 
+                    w-full min-h-screen bg-cover bg-center"
+        ></div>
+        <form
+          className="flex justify-center items-center flex-col w-2/3 gap-3"
+          onSubmit={cadastrarNovoUsuario}
+        >
+          <h2 className="text-slate-900 text-5xl">Cadastrar</h2>
+          <div className="flex flex-col w-full">
             <label htmlFor="nome">Nome</label>
             <input
               type="text"
@@ -82,8 +84,9 @@ async function cadastrarNovoUsuario(e: FormEvent<HTMLFormElement>){
               placeholder="Nome"
               className="border-2 border-slate-700 rounded p-2"
               value={usuario.nome}
-              onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
-
+              onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                atualizarEstado(e)
+              }
             />
           </div>
 
@@ -96,7 +99,9 @@ async function cadastrarNovoUsuario(e: FormEvent<HTMLFormElement>){
               placeholder="Usuário"
               className="border-2 border-slate-700 rounded p-2"
               value={usuario.usuario}
-              onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
+              onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                atualizarEstado(e)
+              }
             />
           </div>
           <div className="flex flex-col w-full">
@@ -108,7 +113,9 @@ async function cadastrarNovoUsuario(e: FormEvent<HTMLFormElement>){
               placeholder="Foto"
               className="border-2 border-slate-700 rounded p-2"
               value={usuario.foto}
-              onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
+              onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                atualizarEstado(e)
+              }
             />
           </div>
           <div className="flex flex-col w-full">
@@ -120,7 +127,9 @@ async function cadastrarNovoUsuario(e: FormEvent<HTMLFormElement>){
               placeholder="Senha"
               className="border-2 border-slate-700 rounded p-2"
               value={usuario.senha}
-              onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
+              onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                atualizarEstado(e)
+              }
             />
           </div>
           <div className="flex flex-col w-full">
@@ -132,7 +141,9 @@ async function cadastrarNovoUsuario(e: FormEvent<HTMLFormElement>){
               placeholder="Confirmar Senha"
               className="border-2 border-slate-700 rounded p-2"
               value={confirmarSenha}
-              onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
+              onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                handleConfirmarSenha(e)
+              }
             />
           </div>
           <div className="flex justify-around w-full gap-8">
@@ -146,21 +157,18 @@ async function cadastrarNovoUsuario(e: FormEvent<HTMLFormElement>){
             <button
               type="submit"
               className="rounded text-white bg-indigo-400 hover:bg-indigo-900 w-1/2 py-2 flex justify-center"
-
             >
-              { isLoading ?
-              <ClipLoader
-            color="#ffffff"
-          size={24}
-        /> :
-        <span>Cadastrar</span>
-        }
+              {isLoading ? (
+                <ClipLoader color="#ffffff" size={24} />
+              ) : (
+                <span>Cadastrar</span>
+              )}
             </button>
           </div>
         </form>
       </div>
     </>
-    )
+  );
 }
 
-export default Cadastro
+export default Cadastro;
