7dcb3d36bc74a493ab7007366bd11379f224704c
Alissa Sousa <alissiasuelen@hotmail.com>
2026-02-20 18:46:55 -0300

    Componentes CardTema e ListaTemas - Estilização

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -4,35 +4,37 @@ import { AuthContext } from "../../contexts/AuthContext";
 
 function Navbar() {
 
-const navigate = useNavigate();
+    const navigate = useNavigate();
 
-const { handleLogout } = useContext(AuthContext)
+    const { handleLogout } = useContext(AuthContext)
 
-function logout() {
-  handleLogout()
-  alert('O Usuário foi desconectado com sucesso!')
-  navigate('/')
-}
+    function logout() {
 
-  return (
-    <>
-      <div className='w-full flex justify-center py-4 bg-indigo-900 text-white'>
+        handleLogout()
+        alert('O Usuário foi desconectado com sucesso!')
+        navigate('/')
+    }
 
-        <div className="container flex justify-between text-lg mx-8">
-          <Link to='/home' className="text=2x1 font-bold">Blog Pessoal</Link>
+    return (
+        <>
+            <div className='w-full flex justify-center py-4
+            			   bg-indigo-900 text-white'>
+            
+                <div className="container flex justify-between text-lg mx-8">
+                    <Link to='/home' className="text-2xl font-bold">Blog Pessoal</Link>
 
-          <div className='flex gap-4'>
-            Postagens
-            Temas
-            Cadastrar tema
-            Perfil
-            Sair
-            <Link to='' onClick={logout} className="houver:underline">Sair</Link>
-          </div>
-        </div>
-      </div>
-    </>
-  )
+                    <div className='flex gap-4'>
+                        Postagens
+                        <Link to='/temas' className='hover:underline'>Temas</Link>
+                        Cadastrar tema
+                        Perfil
+                        Sair
+                        <Link to='' onClick={logout} className='hover:underline'>Sair</Link>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default Navbar
