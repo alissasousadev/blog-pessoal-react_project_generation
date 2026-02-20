6ab5e1c1acbd37d55d144cd9fa9f45aff9402658
Alissa Sousa <alissiasuelen@hotmail.com>
2026-02-20 19:10:46 -0300

    Correção no Service.ts

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -7,4 +7,9 @@ const api = axios.create({
 export const cadastrarUsuario = async (url:string, dados: Object, setDados: Function) => {
     const resposta = await api.post(url, dados)
     setDados(resposta.data)
-}  
+} 
+
+export const login = async (url: string, dados: Object, setDados: Function) => {
+    const resposta = await api.post(url, dados)
+    setDados(resposta.data)
+}
